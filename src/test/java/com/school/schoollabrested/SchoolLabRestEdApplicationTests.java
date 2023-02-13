a936f86c278e2b4ef96de539cf7b9a0f8985f3f9
Edgarc510 <Voltaire0709>
2023-02-13 14:03:53 -0800

    first commit

---
+++ src/test/java/com/school/schoollabrested/SchoolLabRestEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schoollabrested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLabRestEdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
